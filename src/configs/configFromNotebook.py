5921de5c4d2ed9cda76064ecd86251e1dc7838f3
Brent L <brent.limyansky@ucsc.edu>
2023-11-29 15:03:43 -0500

    Filling config
    I fill in most of the configuration file according to the notebook I have been using.

--- src/configs/configFromNotebook.py
+++ src/configs/configFromNotebook.py
@@ -2,24 +2,34 @@
 
 CFG = {
     "data": {
-        "raw_path": "path/here",
+        "raw_path": "./data/raw/arxiv-metadata-oai-snapshot.json",  # Change to proper path
         "processed_path": "path/here"
     },
     "train": {
-        "batch_size": 1,
-        "buffer_size": 1,
-        "epochs": 1,
+        "batch_size": 32,
+        "buffer_size": 1,  # Need to set
+        "epochs": 10,
+        "precision": "mixed_float16",
         "optimizer": {
-            "type": "optimizer name",
-            "step": "step size"
+            "type": "Adam",
+            "learning_rate": 3e-5,
+            "epsilon": 1e-08,
+            "clipnorm": 1.0
         },
         "metrics": ["accuracy"]
     },
     "model": {
         "untuned": {
+            "name": "gpt2",
+            "output_hidden_states": False
         },
         "tuned": {
-        }
-
+        }  # What would I like to save here?
+    },
+    "tokenizer": {
+        "name": "gpt2",
+        "eos_token": "<|endoftext|>",
+        "bos_token": "<|startoftext|>",
+        "pad_token": "<pad>"
     }
 }
