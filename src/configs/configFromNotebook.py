619c7f4970d3e5e0cd10fdbc6db9daaa7e9c9186
Brent L <brent.limyansky@ucsc.edu>
2023-11-29 14:50:42 -0500

    Add placeholders for configuration.

--- src/configs/configFromNotebook.py
+++ src/configs/configFromNotebook.py
@@ -1,3 +1,25 @@
 """Model config replicating ArXiv_PC_Training.ipynb"""
 
-CFG = {}
+CFG = {
+    "data": {
+        "raw_path": "path/here",
+        "processed_path": "path/here"
+    },
+    "train": {
+        "batch_size": 1,
+        "buffer_size": 1,
+        "epochs": 1,
+        "optimizer": {
+            "type": "optimizer name",
+            "step": "step size"
+        },
+        "metrics": ["accuracy"]
+    },
+    "model": {
+        "untuned": {
+        },
+        "tuned": {
+        }
+
+    }
+}
