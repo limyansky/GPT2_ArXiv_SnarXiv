c25f1c4cc6dfc041e4bc93763e4acd7cf68e6f10
limyansky <brent.limyansky@ucsc.edu>
2023-11-30 14:51:19 -0500

    Remove dependency on python-utils

--- src/model/base_model.py
+++ src/model/base_model.py
@@ -1,12 +1,11 @@
 """Abstract base model"""
 
 from abc import ABC, abstractmethod
-from utils.config import Config
 
 class BaseModel(ABC):
     """Abstract Model class that is inherited to all models"""
     def __init__(self, cfg):
-        self.config = Config.from_json(cfg)
+        self.config = cfg
 
     @abstractmethod
     def load_data(self):
