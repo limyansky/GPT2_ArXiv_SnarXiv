4f0775927e54b5dccce65174b490ca061a4dc1b9
limyansky <brent.limyansky@ucsc.edu>
2023-12-11 16:32:04 -0500

    Function to format strings for training

--- src/steps/dataset_processor/data_processor.py
+++ src/steps/dataset_processor/data_processor.py
@@ -20,7 +20,27 @@ def clean_txt(input_str: str) -> str:
         A string which keeps only letters, numbers, (single) spaces, dashes, and colons.
     """
 
-    string = re.sub(r'[^a-zA-Z0-9 - : +]+', '', input_str)
+    string = re.sub(r'[^a-zA-Z0-9 \- : +]+', '', input_str)
     string = re.sub(r' +', ' ', string)
 
     return string
+
+def training_string(input_dict: dict) -> str:
+    """ Generates a string in the proper format to train the model.
+
+    Args:
+        input_dict: A dictionary containing information about each paper.
+
+    Returns
+        A string of the format: 
+            "<|startoftext|> <|categories|> cat1 cat2 <|title|> Paper Title <|endoftext|>"
+    """
+
+    string_template = "<|startoftext|> <|categories|> {} <|title|> {} <|endoftext|>"
+
+    cats = input_dict["categories"]
+    title = input_dict["title"]
+
+    title = clean_txt(title)
+
+    return string_template.format(cats, title)
